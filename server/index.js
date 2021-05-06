b5bf4845a1014f970e0fc48d9f966e1aa80791e2
pholawat-tangsatit <pholawat.tangsatit@hotmail.com>
2021-05-06 19:28:08 +0700

    add postgres deployment, cluster ip, and volume

--- server/index.js
+++ server/index.js
@@ -5,12 +5,38 @@ const app = express();
 
 const PORT = 5000 || process.env.PORT;
 
+const keys = require('./keys');
+const { Pool } = require('pg');
+
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort,
+});
+
+pgClient.on('connect', (client) => {
+    client
+        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+        .catch((err) => console.error(err));
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(logger('tiny'));
 
-app.get('/', (req, res) => {
-    res.send({ msg: 'Success!' });
+app.get('/', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+});
+
+app.get('/add', async (req, res) => {
+    const response = await pgClient.query(
+        `INSERT INTO values values (${Math.floor(Math.random() * 10)})`
+    );
+
+    res.send(response);
 });
 
 app.listen(PORT, () => {
